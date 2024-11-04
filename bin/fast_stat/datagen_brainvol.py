cc6ac42be641d386818e6e35c15c0d95e0cd7a9b
Laroz220 <73316920+Laroz220@users.noreply.github.com>
2024-11-04 22:17:38 +0100

    Add files via upload

---
+++ bin/fast_stat/datagen_brainvol.py
@@ -0,0 +1,35 @@
+import sys
+import csv
+
+def process_stats_file(input_file):
+    data = {}
+    with open(input_file, 'r') as file:
+        lines = file.readlines()
+    for line in lines:
+        if line.startswith('# Measure'):
+            measure, abbreviation, description, volume, unit = line.strip().split(', ')
+            measure = measure.split(' ')[-1] + "_mm3_fastsurfer"  # Add suffix
+            data[measure] = volume
+    return data
+
+def generate_csv(data, output_file):
+    with open(output_file, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=data.keys())
+        writer.writeheader()
+        writer.writerow(data)
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <input_file> <output_file>")
+        sys.exit(1)
+    
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    
+    data = process_stats_file(input_file)
+    generate_csv(data, output_file)
+    
+    print(f"Data has been extracted from {input_file} and written to {output_file}.")
+
+if __name__ == "__main__":
+    main()
---
+++ bin/fast_stat/datagen_wmparc_DKTatlas.py
@@ -0,0 +1,40 @@
+import sys
+import csv
+
+def process_stats_file(input_file):
+    data = {}
+    with open(input_file, 'r') as file:
+        lines = file.readlines()
+    start_processing = False
+    for line in lines:
+        if line.startswith("# ColHeaders"):
+            start_processing = True
+            continue
+        if start_processing and line.strip() != "":
+            parts = line.strip().split()
+            struct_name = parts[4]
+            volume_mm3 = parts[3]
+            data[struct_name + "_mm3_fastsurfer"] = volume_mm3
+    return data
+
+def generate_csv(data, output_file):
+    with open(output_file, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=data.keys())
+        writer.writeheader()
+        writer.writerow(data)
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <input_file> <output_file>")
+        sys.exit(1)
+    
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    
+    data = process_stats_file(input_file)
+    generate_csv(data, output_file)
+    
+    print(f"Data has been extracted from {input_file} and written to {output_file}.")
+
+if __name__ == "__main__":
+    main()
