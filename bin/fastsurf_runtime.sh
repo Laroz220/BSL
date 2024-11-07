49f2145ee8b0505a2c2d72c4ad13cfaab638522e
Laroz220 <73316920+Laroz220@users.noreply.github.com>
2024-11-07 21:43:20 +0100

    Update fastsurf_runtime.sh

--- bin/fastsurf_runtime.sh
+++ bin/fastsurf_runtime.sh
@@ -2,12 +2,15 @@
 
 #Data preparation prior to Free-/Fast, LGI and LST
 
-version=2.2.0
+version=2.3.2
 
 # Set directories
 TEMP_DIR=$C_HOME/Data/TEMP_DIR
 BIDS_dir="$C_HOME/Data/BIDS" # Source directory
 destination_dir="$TEMP_DIR/Surf" # Destination directory
+
+# -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 # Target file
 file="$C_HOME/bin/paths.txt"
@@ -35,36 +38,32 @@ else
 fi
 
 export FREESURFER_HOME="$freesurfer_path"
+export FASTSURFER_HOME="$C_HOME/sys/libraries/FastSurfer"
+
 source $C_HOME/bin/max_jobs.sh
 source $C_HOME/bin/gpu_mode.sh
 
-# Define a function to run FastSurfer Docker container
-run_fastsurfer() {
-    subject_file="$1"
-    # Extract subject information from file names
-    subject=$(basename "$subject_file")
-    subject_no_suffix="${subject%%_T1w-*}"
+export SUBJECTS_DIR="$C_HOME/OUTPUT/FastSurfer"
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
-    # Set this variable to "true" or "false" based on whether you want to use GPU or not
-    USE_GPU=$GPU  # Change to false to disable GPU
+# -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
-    # Construct the command with or without the GPU flag
-    if [ "$USE_GPU" = TRUE ]; then
-        GPU_FLAG="--gpus all"
-    else
-        GPU_FLAG=""
-    fi
+USE_GPU=$GPU  # Change to false to disable GPU
 
-    # Run the docker command
-    docker run $GPU_FLAG -v "$destination_dir/$study":/data \
-        -v "$C_HOME/OUTPUT/FastSurfer:/output" \
-        -v "$freesurfer_path:/freesurfer" \
-        --rm --user $(id -u):$(id -g) deepmi/fastsurfer:latest \
-        --fs_license /freesurfer/license.txt \
-        --t1 "/data/$subject" \
-        --sid "$subject_no_suffix" --sd "/output/$study" \
-        --parallel --3T
-}
+if [ "$USE_GPU" = TRUE ]; then
+    GPU_FLAG="cuda"
+else
+    GPU_FLAG="cpu"
+fi
+
+cd $FASTSURFER_HOME
+
+source ~/miniconda3/bin/activate
+source activate fastsurfer
+
+# -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 for subject_folder in "$destination_dir"/*; do
     study=$(basename "$subject_folder")
@@ -76,17 +75,24 @@ for subject_folder in "$destination_dir"/*; do
     # Iterate through subject files and run FastSurfer Docker containers in groups (optionally)
     for subject_file in "$subject_folder"/*T1w*nii.gz; do
         echo $subject_file
+        
+        subject=$(basename "$subject_file")
+        subject_no_suffix="${subject%%_T1w-*}"
+
+        echo $subject_no_suffix
 
         # NB! If fastsurfer fails at asegdkt during startup, this is most likely due to RAM-issues (RAM or VRAM, depending on CPU/GPU-usage)
         # MRIs with high spatial resolution demand a very high amount of RAM/VRAM
 
-        # Modify the integer to the left of "==" to define batch size
-        run_fastsurfer "$subject_file" &
+        ./run_fastsurfer.sh --sid sub-0001 --sd $SUBJECTS_DIR/$study --t1 $subject_file --device $GPU_FLAG &
         ((++count % $MAX_JOBS == 0)) && wait
     done
 done
 
 # Wait for all background jobs to finish
 wait
+
+# -------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 echo -e "\n[INFO] FastSurfer [v$version]: Processing completed for all subjects""!""\n\n****************************************************************************************************\n"
